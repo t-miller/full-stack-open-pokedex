9aab47bcb61a491f673a8352cddcccce418a5238
Tyler Miller <tyler.rw.miller@gmail.com>
2021-11-26 12:13:03 -0500

    Add cypress configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,13 @@ module.exports = {
     "browser": true,
     "node": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "plugin:cypress/recommended"
   ],
   "parserOptions": {
     "ecmaFeatures": {
@@ -17,7 +19,9 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", 
+    "jest", 
+    "cypress"
   ],
   "rules": {
     "indent": [
